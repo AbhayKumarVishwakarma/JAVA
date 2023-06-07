7abb76b9f35a294422ff61dda501f4bf6c3793fe
AbhayKumarVishwakarma <3492akv@gmail.com>
2023-06-07 08:48:12 +0530

    add

---
+++ SB-201/Class project/springsecurity/src/main/java/com/example/SpringsecuritywithdefaultconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritywithdefaultconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritywithdefaultconfigurationApplication.class, args);
+	}
+
+}
---
+++ SB-201/Class project/springsecurity/src/main/java/com/example/configure/ProjectSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain masaiSecurityFilter(HttpSecurity http) throws Exception {
+
+            http.authorizeHttpRequests( auth -> {
+//                auth.anyRequest().permitAll();
+                auth.requestMatchers("/admin","/welcome").permitAll().anyRequest().authenticated();
+            }).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+
+            return http.build();
+    }
+//
+    @Bean
+    public InMemoryUserDetailsManager userDetails(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        UserDetails admin = User.withUsername("admin").password("12345").authorities("admin").build();
+        UserDetails user = User.withUsername("user").password("12345").authorities("read").build();
+        manager.createUser(admin);;
+        manager.createUser(user);
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
